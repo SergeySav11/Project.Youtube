49cb8d789f2985b655ad86459f8992f891273ac5
Arinasavel <arinasavel2007@gmail.com>
2022-02-06 18:00:00 +0300

    downloading_2

--- playlist_downloading.py
+++ playlist_downloading.py
@@ -1,6 +1,47 @@
-from tqdm import tqdm,tqdm_gui,trange
-from time import sleep
 
-for i in trange(100,leave =True,mininterval =1,ncols = 100,unit_scale = 10):
-    sleep(0.1)
+from tkinter import *
+from tkinter.ttk import Combobox
 
+def quality(types):
+    global combo
+    combo = Combobox(window)
+    combo['values'] = (types)
+    combo.current(0)
+    combo.grid(column=0, row=1)
+
+
+def download():
+    qual = combo.get()
+
+    pass
+
+    print(link) #В переменной !link! хранится ссылка на видео
+    print(qual) #В переменной !qual! хранится качество видео
+
+
+def qualitization():
+    global link
+    link = txt.get()
+
+    pass
+
+    qualit = [480,560,1080,2500,1000,800] #Сюда укажите качества видео массивом
+
+    quality(qualit)
+    txt.destroy()
+    lbl.configure(text = "Укажите качество видео:")
+    btn.configure(command=download)
+
+
+
+
+window = Tk()
+window.title("Youtube Downloader")
+window.geometry('484x300')
+lbl = Label(window, text="Вставьте ссылку на видео:", font=("Arial Bold", 10))
+lbl.grid(column=0, row=0)
+txt = Entry(window,width=80)
+txt.grid(column=0, row=1)
+btn = Button(window, text="Скачать!", font=("Arial Bold", 10), command=qualitization)
+btn.grid(column=0, row=2)
+window.mainloop()
--- vid_downloading.py
+++ vid_downloading.py
@@ -1,17 +1,26 @@
 from pytube import YouTube
+from tqdm import tqdm,tqdm_gui,trange
+from time import sleep
+
+def download(url):
+    my_video = YouTube(url)
+    resolutions = my_video.streams
+    vid = list(enumerate(resolutions))
+    #print(my_video.title)
+    #print(my_video.thumbnail_url)
+
+    #Создание списка с качествами
+    res = []
+    for i in vid:
+        res = res.append(str(i)[46:58])
+    print()
+    #Переменная хранящая выбраное качество
+    strm = input()
+    #Cкачивание видео
+    resolutions[strm].download()
+    #Возвращение списка с качествами,
+    return int(res)
 
 
-url = str(input())
-my_video = YouTube(url)
-resolutions = my_video.streams
-vid = list(enumerate(resolutions))
 
-b = 1
-for i in vid:
-    print(str(i)[46:58])
-    b += 1
-print()
-strm = int(input())
-
-resolutions[strm].download()
-print('Загрузка завершена')
+download('https://www.youtube.com/watch?v=LmEcyQnfpDA',)
